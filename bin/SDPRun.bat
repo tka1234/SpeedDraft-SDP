129477dda12f60eb6ba94ff90e3ef429db06369d
T. Alexander <t.alex@outlook.com>
2018-11-07 22:23:24 -0500

    Basic Java check
    Will improve later

--- bin/SDPRun.bat
+++ bin/SDPRun.bat
@@ -1,3 +1,9 @@
 @ECHO OFF
+IF EXIST "C:\Program Files\Java\jre1.8.0_181" (
+ECHO Recommended Java version found.
 java SDPMain
-pause
+)
+IF NOT EXIST "C:\Program Files\Java\jre1.8.0_181" (
+echo Recommended Java version 1.8.0_181 not installed.
+)
+PAUSE
